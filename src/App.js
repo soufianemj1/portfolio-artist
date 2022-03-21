f743fc662e77a595f4966cc106656e2d9e90a542
soufianemj1 <elmajati.s@gmail.com>
2022-03-21 09:11:45 +0100

    contact

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Landing from '../src/components/landing';
 import Gallery from '../src/components/Gallery';
+import Contact  from './components/Contact';
 
 function App() {
   return (
     <div>
       < Landing />
       < Gallery />
+      < Contact />
     </div>
-  );
+    )
+    
 }
 
 export default App;
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,44 @@
-import Caroussel from "./Caroussel"
+
+import Image1 from "../images/Image1.jpg"
+import image2 from "../images/image2.jpg"
+import image3 from "../images/image3.jpg"
+import image4 from "../images/image4.jpg"
+import image5 from "../images/image5.jpg"
+
 const Gallery = () => {
   return (
-    <div className="min-h-screen">
-        <div>
-            <h1 className="text-center text-2xl font-semi-bold">Welcome to my gallery, here's some of my art</h1>
-        </div>
-        <div>
-        <Caroussel/>
+    // <div className="min-h-screen"> 
+<div className="max-w-screen min-h-screen p-4 dark:bg-gray-800  px-4 sm:px-6 lg:px-8 py-20 ">
+    <div className="relative">
+        <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+            <div className="lg:col-start-2 lg:max-w-2xl ml-auto">
+                
+                <h4 className="mt-2 text-2xl leading-8 font-extrabold text-gray-900 dark:text-white sm:text-3xl sm:leading-9">
+                    Here is some of my art work.
+                </h4>
+                <p className="mt-4 text-lg leading-6 text-gray-500 dark:text-gray-300">
+                   I have a lot of artworks, but I choosed these four to give you an idea about what i do, you can see a lot of my work on Instagram.
+                </p>
+                
+            </div>
+            <div className="mt-10 lg:-mx-4 relative relative-20 lg:mt-0 lg:col-start-1">
+                <div className="relative space-y-4">
+                    <div className="flex items-end justify-center mx-20 lg:justify-start space-x-4">
+                        <img className="rounded-lg shadow-lg w-32 md:w-56" width="200" src={Image1} alt="1"/>
+                        <img className="rounded-lg shadow-lg w-40 md:w-64" width="260" src={image2} alt="2"/>
+                    </div>
+                    <div className="flex items-start justify-center lg:justify-start space-x-4 ml-2">
+                        <img className="rounded-lg shadow-lg w-24 md:w-40" width="170" src={image3} alt="3"/>
+                        <img className="rounded-lg shadow-lg w-32 md:w-56" width="200" src={image4} alt="4"/>
+                        <img className="rounded-lg shadow-lg w-32 md:w-56" width="200" src={image5} alt="4"/>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+</div>
+
+    // </div>
   )
 }
 
